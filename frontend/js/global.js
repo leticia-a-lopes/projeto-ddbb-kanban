a7d8523fad8e16d3dc987ca768fa2322734bb669
figdavi <figueiredodavi.dev@proton.me>
2025-12-11 16:39:39 -0300

    feat: adicionar logica de criação e anexação de cards em quadros específicos

--- frontend/js/global.js
+++ frontend/js/global.js
@@ -1,3 +1,5 @@
+const API_URL = "http://localhost:3000";
+
 function carregarHeader() {
     const headerHTML = `
         <div class="logo-area" onclick="window.location.href='quadro_principal.html'" style="cursor: pointer;">
@@ -47,93 +49,146 @@ window.addEventListener("click", function (event) {
 // executa assim que a página carregar
 document.addEventListener("DOMContentLoaded", carregarHeader);
 
-function adicionarCard(
-    colunaId,
-    nomeCliente,
-    idCliente,
-    nomeUsuario,
-    idUsuario,
-    corIconeUsuario,
-    data = null,
-    hora = null
-) {
-    // Não use HTML id=idCliente ou idUsuario, visto que podem ter o mesmo valor (id pode ser 1 na tabela de cliente e usuario e id no html deve ser único).
-    // Podemos usar id="cliente-${id}", mas isso dificulta retornar o id para o backend, seria necessário fazer strip da da parte numerica toda vez.
-    // Portanto, usaremos um atributo data-id-cliente/data-id-usuario, desse modo pegamos o melhor dos dois mundos.
-    // Ref: https://www.reddit.com/r/webdev/comments/1lo3deu/html_identifiers_for_dynamic_data/
+// Usaremos um atributo data-id-cliente/data-id-usuario, ao invés de id.
+// Ref: https://www.reddit.com/r/webdev/comments/1lo3deu/html_identifiers_for_dynamic_data/
 
-    const coluna = document.getElementById(colunaId);
-
-    const userIconHTML = `
-        <div class="card-user-icon ${corIconeUsuario}" data-tooltip="${nomeUsuario}" data-id-usuario="${idUsuario}">
+// Funções de Criar Card
+function criarIconeUsuario(usuario) {
+    return `
+        <div class="card-user-icon" 
+             style="background-color: ${usuario.corIcone};"
+             data-tooltip="${usuario.nome_usuario}"
+             data-id-usuario="${usuario.id}">
             <svg><use href="#icon-user"></use></svg>
         </div>
     `;
+}
 
-    let html = "";
+function criarCard(cliente, usuario) {
+    const card = document.createElement("div");
+    card.className = "card-aluno";
+    card.dataset.idCliente = cliente.id;
+    card.addEventListener("click", () => abrirEditar(false));
 
-    if (!data) {
-        // TIPO 1: em contato
-        html = `
-            <div class="card-aluno" data-id-cliente="${idCliente}" onclick="abrirEditar(false)">
-                <div class="card-header">
-                    <svg class="card-icon"><use href="#icon-3dots"></use></svg>
-                    <div class="name-icon-row">
-                        <span class="card-nome" style="margin: 0;">${nomeCliente}</span>
-                        ${userIconHTML}
+    const iconeUsuario = criarIconeUsuario(usuario);
+
+    if (cliente.dataAgendamento != null) {
+        card.innerHTML = `
+            <div class="card-header">
+                <svg class="card-icon"><use href="#icon-3dots"></use></svg>
+                <span class="card-nome">${cliente.nome_cliente}</span>
+            </div>
+            <div class="card-footer-tags">
+                <div class="card-tags-container">
+                    <div class="card-tag">
+                        <svg><use href="#icon-calendar"></use></svg>
+                        <span>${cliente.agendamento.diaAgendamento}</span>
+                    </div>
+                    <div class="card-tag tag-time">
+                        <svg><use href="#icon-clock"></use></svg>
+                        <span>${cliente.agendamento.horaAgendamento}</span>
                     </div>
                 </div>
+                ${iconeUsuario}
             </div>
         `;
-    } else {
-        // TIPO 2: agendado
-        html = `
-            <div class="card-aluno" data-id-cliente="${idCliente}" onclick="abrirEditar(true)">
-                <div class="card-header">
-                    <svg class="card-icon"><use href="#icon-3dots"></use></svg>
-                    <span class="card-nome">${nomeCliente}</span>
+    } else if (cliente.estaArquivado) {
+        card.innerHTML = `
+            <div class="card-header">
+                <svg class="card-icon" style="color: ${usuario.corIcone};">
+                    <use href="#icon-3dots"></use>
+                </svg>
+                <div class="name-icon-row">
+                    <span class="card-nome">${cliente.nome_cliente}</span>
+                    ${iconeUsuario}
                 </div>
-                <div class="card-footer-tags">
-                    <div class="card-tags-container">
-                        <div class="card-tag">
-                            <svg><use href="#icon-calendar"></use></svg><span>${data}</span>
-                        </div>
-                        <div class="card-tag tag-time">
-                            <svg><use href="#icon-clock"></use></svg><span>${hora}</span>
-                        </div>
-                    </div>
-                    ${userIconHTML}
+            </div>
+            ${
+                cliente.motivoDesistencia
+                    ? `
+                <div class="card-motivo">
+                    <small>Motivo: ${cliente.motivoDesistencia}</small>
+                </div>
+                `
+                    : ""
+            }
+        `;
+    } else {
+        card.innerHTML = `
+            <div class="card-header">
+                <svg class="card-icon"><use href="#icon-3dots"></use></svg>
+                <div class="name-icon-row">
+                    <span class="card-nome">${cliente.nome_cliente}</span>
+                    ${iconeUsuario}
                 </div>
             </div>
         `;
     }
 
-    coluna.innerHTML += html;
+    return card;
 }
 
-async function popularQuadros() {
-    try {
-        const response = await fetch("http://localhost:3000/cards");
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
+function adicionarCard(card, coluna) {
+    if (!coluna) {
+        console.error(`Coluna não encontrada: ${coluna}`);
+        return;
+    }
+    coluna.appendChild(card);
+}
 
-        // Provisório, necessário criação de novos campos na api e bd (caso for implementar histórico para etapa em arquivados).
-        // TODO: substituir "lista-contato" por cliente.status, "Clara Maria" por usuario.nome e "red" por usuario.color
-        for (const cliente of data) {
-            adicionarCard(
-                "lista-contato",
-                cliente.nome,
-                cliente.id,
-                "Clara Maria",
-                Math.floor(Math.random() * 100),
-                "red"
+async function carregarQuadroPrincipal() {
+    try {
+        const quadroResponse = await fetch(`${API_URL}/quadro/`);
+        if (!quadroResponse.ok) {
+            throw new Error(
+                `Erro HTTP ao fazer fetch de quadros! status: ${quadroResponse.status}`
             );
         }
-        adicionarDragDrop();
+        const quadros = await quadroResponse.json();
+
+        for (const quadro of quadros) {
+            if (quadro.nome_quadro == "quadro_principal") {
+                configurarDragDropQuadro();
+                popularQuadro(quadro);
+                break;
+            }
+        }
     } catch (error) {
-        console.error("Fetch error:", error);
+        console.error("Erro ao carregar quadro inicial:", error);
+    }
+}
+
+async function popularQuadro(quadro) {
+    try {
+        const clienteResponse = await fetch(`${API_URL}/quadro/${quadro.id}`);
+        if (!clienteResponse.ok) {
+            throw new Error(
+                `Erro HTTP ao fazer fetch de clientes no quadro "${quadro}"! status: ${clienteResponse.status}`
+            );
+        }
+        const clientes = await clienteResponse.json();
+
+        for (const cliente of clientes) {
+            const usuarioResponse = await fetch(
+                `${API_URL}/usuario/${cliente.id_usuario}/`
+            );
+
+            if (!usuarioResponse.ok) {
+                throw new Error(
+                    `Erro HTTP ao fazer fetch de usuarios! status: ${usuarioResponse.status}`
+                );
+            }
+
+            const usuario = await usuarioResponse.json();
+
+            const card = criarCard(cliente, usuario);
+            const coluna = "lista-" + cliente.colunaAtual;
+
+            adicionarCard(card, coluna);
+            configurarDragDropCard(card);
+        }
+    } catch (error) {
+        console.error("Erro ao popular quadros:", error);
     }
 }
 
@@ -143,67 +198,29 @@ async function popularQuadros() {
 let activeDragCardId = null;
 
 // TODO: Integrar com endpoint de mudança de status
-function adicionarDragDrop() {
-    cards = document.querySelectorAll(".card-aluno[data-id-cliente]");
-    columns = document.querySelectorAll(".kanban-column");
+function configurarDragDropCard(card) {
+    card.setAttribute("draggable", "true");
 
-    cards.forEach((card) => {
-        card.setAttribute("draggable", "true");
+    // Armazena ID para uso no drop
+    card.addEventListener("dragstart", (event) => {
+        if (!card.hasAttribute("data-id-cliente") || !card.dataset.idCliente) {
+            console.error(
+                "Drag card: card não possui [data-id-cliente] válido:",
+                card
+            );
+            return;
+        }
+        card.classList.add("dragging");
+        event.dataTransfer.effectAllowed = "move";
 
-        // Armazena ID para uso no drop
-        card.addEventListener("dragstart", (event) => {
-            if (
-                !card.hasAttribute("data-id-cliente") ||
-                !card.dataset.idCliente
-            ) {
-                console.error(
-                    "Drag card: card não possui [data-id-cliente] válido:",
-                    card
-                );
-                return;
-            }
-            card.classList.add("dragging");
-            event.dataTransfer.effectAllowed = "move";
-
-            event.dataTransfer.setData("card-aluno", "");
-            activeDragCardId = card.dataset.idCliente;
-        });
-
-        // Limpa ID armazenado após a ação de arrastar terminar
-        card.addEventListener("dragend", () => {
-            activeDragCardId = null;
-            card.classList.remove("dragging");
-        });
+        event.dataTransfer.setData("card-aluno", "");
+        activeDragCardId = card.dataset.idCliente;
     });
 
-    columns.forEach((column) => {
-        column.addEventListener("dragover", moveDragPlaceholder);
-
-        // Remove o placeholder quando o card sair da área
-        column.addEventListener("dragleave", (event) => {
-            if (column.contains(event.relatedTarget)) {
-                return;
-            }
-
-            const dragPlaceholder = column.querySelector(".drag-placeholder");
-            dragPlaceholder?.remove();
-        });
-
-        // Insere card na área de drop (inserimos ele acima do placeholder e logo em seguida removemos o placeholder)
-        column.addEventListener("drop", (event) => {
-            event.preventDefault();
-
-            const draggedCard = document.querySelector(
-                `[data-id-cliente="${activeDragCardId}"]`
-            );
-
-            const dragPlaceholder = column.querySelector(".drag-placeholder");
-            if (!dragPlaceholder) return;
-            draggedCard.remove();
-            cardList = column.querySelector(".card-list");
-            cardList.insertBefore(draggedCard, dragPlaceholder);
-            dragPlaceholder.remove();
-        });
+    // Limpa ID armazenado após a ação de arrastar terminar
+    card.addEventListener("dragend", () => {
+        activeDragCardId = null;
+        card.classList.remove("dragging");
     });
 }
 
@@ -289,4 +306,38 @@ function moveDragPlaceholder(event) {
     );
 }
 
-document.addEventListener("DOMContentLoaded", popularQuadros());
+function configurarDragDropQuadro() {
+    columns = document.querySelectorAll(".kanban-column");
+
+    columns.forEach((column) => {
+        column.addEventListener("dragover", moveDragPlaceholder);
+
+        // Remove o placeholder quando o card sair da área
+        column.addEventListener("dragleave", (event) => {
+            if (column.contains(event.relatedTarget)) {
+                return;
+            }
+
+            const dragPlaceholder = column.querySelector(".drag-placeholder");
+            dragPlaceholder?.remove();
+        });
+
+        // Insere card na área de drop (inserimos ele acima do placeholder e logo em seguida removemos o placeholder)
+        column.addEventListener("drop", (event) => {
+            event.preventDefault();
+
+            const draggedCard = document.querySelector(
+                `[data-id-cliente="${activeDragCardId}"]`
+            );
+
+            const dragPlaceholder = column.querySelector(".drag-placeholder");
+            if (!dragPlaceholder) return;
+            draggedCard.remove();
+            cardList = column.querySelector(".card-list");
+            cardList.insertBefore(draggedCard, dragPlaceholder);
+            dragPlaceholder.remove();
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", carregarQuadroPrincipal());
